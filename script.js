edb31d91805c4e4d6b28e0a6eb3123056d68cdf7
kelly-igbokwe-kuti <kelly.igbokwekuti@leerling.sjsp.be>
2026-09-18 11:35:01 +0200

    Update script.js
    holy code

--- script.js
+++ script.js
@@ -3,5 +3,23 @@ window.addEventListener('DOMContentLoaded', () => {
   setTimeout(() => {
     titleImage.classList.add('active');
   }, 400);
-  
+  const track = document.querySelector('.scroll-track');
+  let currentPage = 0;
+  let isAnimating = false;
+  const scrollThreshold = 50;
+  window.addEventListener('wheel', (event) => {
+    if (isAnimating) return;
+    if (Math.abs (event.deltaY) < scrollThreshold === 0) {
+      currentPage = 1;
+      isAnimating = true;
+      track.style.transform = 'translateY(-100vh)';
+    } else if (event.deltaY < 0 &&currentPage === 1) {
+      currentPage = 0;
+      isAnimating =  true;
+      track.style.transform = 'translateY(0vh)';
+    }
+    setTimeout(() => {
+      isAnimating = false;
+    }, 1200);
+  });
 });
